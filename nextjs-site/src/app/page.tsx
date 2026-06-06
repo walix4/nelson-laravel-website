85bd1f780f69baf2d2f02aef1d7db8422ec3a9cc
waleed tahir <waleedtahir2222@gmail.com>
2026-06-06 18:01:27 +0500

    DrayToll: classic 4-color Google Play icon (cyan/green/yellow/red)

--- nextjs-site/src/app/page.tsx
+++ nextjs-site/src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
                 </a>
                 {/* Google Play badge */}
                 <a href="#" className="inline-flex items-center gap-2.5 rounded-md h-[54px] w-full pl-3 pr-4 bg-white/[0.12] hover:bg-white/[0.26] border border-white/15 backdrop-blur-md transition-colors duration-200">
-                  <svg width="22" height="24" viewBox="0 0 512 512" aria-hidden="true" className="shrink-0"><path fill="#4895F6" d="M60 40 220 256 60 472z"/><path fill="#1FBE5B" d="M60 40 340 188 220 256z"/><path fill="#EE4339" d="M60 472 340 324 220 256z"/><path fill="#FFBA00" d="M340 188 L424 236 Q450 252 450 256 Q450 260 424 276 L340 324 L220 256 Z"/></svg>
+                  <svg width="24" height="24" viewBox="20 18 64 64" aria-hidden="true" className="shrink-0"><path fill="#00C3FF" d="M24 22 53 50 24 78 Z"/><path fill="#00D95F" d="M24 22 62 41 53 50 Z"/><path fill="#FF3D44" d="M24 78 62 59 53 50 Z"/><path fill="#FFCE00" d="M62 41 L75 47 Q80 50 75 53 L62 59 L53 50 Z"/></svg>
                   <span className="leading-none text-white text-left whitespace-nowrap"><span className="block text-[8.5px] uppercase tracking-[0.14em] opacity-90">Get it on</span><span className="block text-[14px] font-semibold tracking-tight">Google Play</span></span>
                 </a>
               </div>
