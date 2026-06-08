8278b15d0bf3810e355dc9659b04429d5a807eb3
waleed tahir <waleedtahir2222@gmail.com>
2026-06-08 07:46:37 +0500

    drayow.com: favicon -> filled navy/yellow gauge badge (v4)

--- nextjs-site/src/app/layout.tsx
+++ nextjs-site/src/app/layout.tsx
@@ -12,9 +12,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="icon" href={`${BASE}/favicon.ico?v=3`} sizes="any" />
-        <link rel="icon" type="image/png" href={`${BASE}/favicon.png?v=3`} />
-        <link rel="apple-touch-icon" href={`${BASE}/apple-touch-icon.png?v=3`} />
+        <link rel="icon" href={`${BASE}/favicon.ico?v=4`} sizes="any" />
+        <link rel="icon" type="image/png" href={`${BASE}/favicon.png?v=4`} />
+        <link rel="apple-touch-icon" href={`${BASE}/apple-touch-icon.png?v=4`} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
